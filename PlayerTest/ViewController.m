e9ac471a27dc25b669588fe03b918be30fb6085f
StevenQzl <zhaobo@Lis-Mac-mini.local>
2016-10-10 09:57:02 +0800

    Revert "3"
    This reverts commit aef1b0df72b0c15cdf75d83a3b35ca8164caf55a.

--- PlayerTest/ViewController.m
+++ PlayerTest/ViewController.m
@@ -23,12 +23,6 @@
 - (XCAVPlayerView *)playerView{
     if (!_playerView) {
         _playerView = [[XCAVPlayerView alloc]init];
-        UIView *playBgView = [[UIView alloc]initWithFrame:CGRectMake(0, 20.0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.width * 0.58)];
-        [self.view addSubview:playBgView];
-        self.playerView.frame = playBgView.bounds;
-        [playBgView addSubview:self.playerView];
-        self.playerView.playerUrl = [NSURL URLWithString:@"http://api.feixiong.tv/Api/Base/getShortM3u8?params=%7B%22data%22%3A%7B%22id%22%3A281%2C%22stream_type%22%3A%22hd2%22%2C%22ykss%22%3A%22%22%7D%7D"];
-        [self.playerView play];
     }
     return _playerView;
 }
